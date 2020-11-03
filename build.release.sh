4f4c182e75710f3c850c4de3d0165f82f6434454
afmika <afmichael73@gmail.com>
2020-11-04 00:08:37 +0300

    console removed

---
+++ build.release.sh
@@ -0,0 +1,11 @@
+# We define the includes which are not specific to the project to avoid compilation errors
+# Wall means show warnings
+g++ -mwindows  -std=c++11 -Wall -s -I include -I "./include" -c classes/*.h
+g++ -mwindows  -std=c++11 -Wall -s -I include -I "./include" -c classes/*.cpp
+g++ -mwindows  -std=c++11 -Wall -s -I include -I "./include" -c *.cpp
+
+# We define the result of the build + obj files to link + the libraries included in / lib
+g++ -mwindows -std=c++11 -L "./lib" -o "./bin/puzzle" *.o -s -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
+
+# starts the application
+./bin/puzzle
